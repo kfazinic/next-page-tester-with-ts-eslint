1bcb37505dd9d3973f85518d420e0f5261bb8a9c
Kristina Fazinić <kfazinic@outlook.com>
2020-11-27 13:17:16 +0100

    Add next-page-tester

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import { render, fireEvent } from '../testUtils'
-import { Home } from '../../pages/index'
+// import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { getPage } from 'next-page-tester'
+
+// import Home from '../../pages/index'
+// import { render } from '@testing-library/react'
+
+// describe('Home page', () => {
+//   it('clicking button triggers alert', () => {
+//     const { getByText } = render(<Home />, {})
+//     expect(getByText('Hi'))
+//   })
+// })
 
 describe('Home page', () => {
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
-    expect(asFragment()).toMatchSnapshot()
-  })
+  it('renders home page', async () => {
+    const { page } = await getPage({
+      route: '/',
+    })
 
-  it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
-    window.alert = jest.fn()
-    fireEvent.click(getByText('Test Button'))
-    expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
+    render(page)
+    expect(screen.getByText('Hi')).toBeInTheDocument()
   })
 })
